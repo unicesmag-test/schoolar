9ee82e55283cefca454ddfb48bd23bce145a93d8
Joan <joan@mail.com>
2025-03-28 18:09:04 -0500

    Register new user request. Do not repeat email validation

---
+++ config/database.php
@@ -0,0 +1,25 @@
+<?php
+    //Config connection
+    $host       = "localhost";
+    $port       = "5432";
+    $dbname     = "schoolar";
+    $user       = "postgres";
+    $password   = "unicesmag";
+
+    //Create connection
+    $conn = pg_connect("
+        host=$host
+        port=$port
+        dbname=$dbname
+        user=$user
+        password=$password
+    ");
+
+    if(!$conn){
+        die("Connection error: " . pg_last_error());
+    }else{
+        echo "Success connection";
+    }
+
+    pg_close();
+?>
