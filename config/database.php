859504483b25a60cee14d708e182b3e667cf9107
Joan <joan@mail.com>
2025-04-04 18:09:21 -0500

    Supabase database connection and configuration

--- config/database.php
+++ config/database.php
@@ -1,10 +1,18 @@
 <?php
     //Config connection
+    $host       = "aws-0-us-east-1.pooler.supabase.com";
+    $port       = "6543";
+    $dbname     = "postgres";
+    $user       = "postgres.apqoncldjmchzrgimabr";
+    $password   = "unicesmag@@";
+    
+    /*
     $host       = "localhost";
     $port       = "5432";
     $dbname     = "schoolar";
     $user       = "postgres";
     $password   = "unicesmag";
+    */
 
     //Create connection
     $conn = pg_connect("
