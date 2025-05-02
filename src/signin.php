276df803414153c9da0450afe687f3c4bc75ab63
Joan <joan@mail.com>
2025-05-02 18:07:08 -0500

    Session in login

--- src/signin.php
+++ src/signin.php
@@ -1,23 +1,28 @@
 <?php
     include('../config/database.php');
 
+    session_start();
+
+    if(isset($_SESSION['user_id'])) {
+        header('Refresh: 0; URL=http://localhost/schoolar/src/home.php');
+    }
+
     $email = $_POST['e_mail'];
     $passw = $_POST['p_sswd'];
+    $enc_pass = sha1($passw);
 
     $sql = "
         SELECT 
-            --id,
-            --email,
-            --password,
+            id,
             COUNT(id) as total
         FROM 
             users
         WHERE
             email = '$email' and
-            password = '$passw' and
+            password = '$enc_pass' and
             status = true
         GROUP BY
-            id;
+            id
     ";
 
     $res = pg_query($conn, $sql);
@@ -25,7 +30,9 @@
     if($res){
         $row = pg_fetch_assoc($res);
         if($row['total'] > 0){
-            echo "Login OK";
+            //echo "Login OK";
+            $_SESSION['user_id'] = $row['id'];
+            header('Refresh: 0; URL=http://localhost/schoolar/src/home.php');
         }else{
             echo "Login failed";
         }
